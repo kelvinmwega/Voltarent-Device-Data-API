a32674a12606f2c0db9491c69b37a3162dc74e62
vunyx <kmwega@gmail.com>
2020-02-20 12:27:14 +0300

    update device timestamp

--- src/main/java/com/vunyx/devicedataapiserver/DataProcessors/SaveDevData.java
+++ src/main/java/com/vunyx/devicedataapiserver/DataProcessors/SaveDevData.java
@@ -1,8 +1,14 @@
 package com.vunyx.devicedataapiserver.DataProcessors;
 
+import com.cloudant.client.api.Database;
+import com.cloudant.client.api.query.QueryBuilder;
+import com.cloudant.client.api.query.QueryResult;
+import com.cloudant.client.api.query.Selector;
 import com.google.gson.JsonObject;
 import com.vunyx.devicedataapiserver.database.DbConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.cloudant.client.api.query.Expression.eq;
 
 public class SaveDevData {
 
@@ -12,8 +18,25 @@ public class SaveDevData {
     public boolean SavaDeviceData(JsonObject devData, String db){
         try {
             dbConfig.database(db).save(devData);
+            updateDeviceData(devData, "devicesdb");
             return true;
         } catch (Exception e){
+            return false;
+        }
+    }
+
+    private boolean updateDeviceData(JsonObject devData, String db){
+        try {
+            Database devdb = dbConfig.database(db);
+//            JsonObject device = devdb.find(JsonObject.class, devData.get("deviceid").getAsString());
+            Selector selector =  eq("id",devData.get("deviceid").getAsString());
+            QueryResult<JsonObject> devices = devdb.query(new QueryBuilder(selector).
+                            build(), JsonObject.class);
+            JsonObject device = devices.getDocs().get(0);
+            device.addProperty("latestupdate", devData.get("timestamp").getAsString());
+            devdb.update(device);
+            return true;
+        } catch (Exception e) {
             return false;
         }
     }
